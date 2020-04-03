d82d4519c5f6ab37c3bbe4005de85076cc16dd0c
Mell <mellkayrusson@gmail.com>
2020-04-03 17:08:18 +0100

    silly yoga

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,74 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+// import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const randomPositions = [
+  "Downward Dog",
+  "Child Pose",
+  " Easy Pose",
+  "Tree",
+  "Boat",
+]
+const randomObject = []
+const randomLimb = ["Hand", "Foot", "Head", "Shoulder", "Knee", "Back"]
+
+// PLAN
+// Generate a random yoga pose and hold a random object with a random limb
+// input 5 nearby random objects
+// random position
+// random object
+// random limb
+// input for objects
+// button to roll
+// maybe an image of the yoga position?
+function IndexPage() {
+  const [isClicked, setIsClicked] = useState(false)
+  const [pos, setPos] = useState("")
+  const [obj, setObj] = useState("")
+  const [limb, setLimb] = useState("")
+  return (
+    <div>
+      <h1>Silly Yoga</h1>
+      <label>Enter 5 objects that are nearby:</label>
+      <input
+        placeholder="separate with a comma"
+        onBlur={e => {
+          let objs = e.target.value
+          let x = objs.split(",")
+          for (let i = 0; i < x.length; i++) {
+            randomObject.push(x[i])
+          }
+        }}
+      />
+      <input
+        type="submit"
+        onClick={() => {
+          console.log(randomObject)
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          setPos(
+            randomPositions[Math.floor(Math.random() * randomPositions.length)]
+          )
+          setObj(randomObject[Math.floor(Math.random() * randomObject.length)])
+          setLimb(randomLimb[Math.floor(Math.random() * randomLimb.length)])
+          setIsClicked(true)
+        }}
+      >
+        Roll!
+      </button>
+      {isClicked && (
+        <p>
+          You must do {pos} and balance a {obj} on your {limb}
+        </p>
+      )}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  )
+}
 
 export default IndexPage
